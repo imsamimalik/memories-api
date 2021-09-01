f12b0113c65b39d3a5815f7f6251b31baaeaa9f3
imsamimalik <salaarwali.malik@gmail.com>
2021-09-01 06:22:54 +0500

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Memories API");
+});
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@testcluster.wrv2f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server Running on Port: http://localhost:${PORT}`)
+        )
+    )
+    .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
